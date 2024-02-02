518cd3d5fe6932634564ba851c999ee2984ede00
sangmesh panchal <you@example.com>
2024-02-02 21:59:06 +0530

    start

---
+++ xcounter/src/App.js
@@ -0,0 +1,24 @@
+
+import React, { useState } from 'react';
+
+function CounterApp() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default CounterApp;
